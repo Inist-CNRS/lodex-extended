28d5dd25904b0dd0891ca16a65a97c565c77ab16
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-04-03 09:07:35 +0200

    like lodex-workers

--- generate-dotenv.js
+++ generate-dotenv.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const environnement = require('./config.json');
+const { environnement } = require('./config.json');
 const vars = Object.keys(environnement)
     .filter(key => typeof environnement[key] !== 'object')
     .map(key => `${key}=${JSON.stringify(environnement[key])}`)
